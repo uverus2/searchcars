9adbae29ea87c336b5b17f483c75a03b45b9bbc1
uverus2 <2ruzhk43@solent.ac.uk>
2018-03-08 21:01:15 +0000

    fix for js click show function on adv search

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function click() {
 
     var x = document.getElementById("myDIV");
 
-    if (x.style.display === "none ") {
+    if (x.style.display === "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
